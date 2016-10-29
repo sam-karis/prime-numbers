c37bbf82ba8f73fb2e50237a64fee23c28201a22
unknown <samkaris75@gmail.com>
2016-10-29 01:37:57 -0700

    changed the whole logic to use a double for loop within the same function instead of using a support function that test if a number is a function

--- prime_num.py
+++ prime_num.py
@@ -1,24 +1,9 @@
-"""
-this is my first function for boot camp 11
-that generate prime generate prime numbers
-"""
-def generate_prime_numbers(n):
-	prime_list = []
-	for num in range(2,n+1):
-		if test_if_is_a_prime(num):
-			prime_list.append(num)
-	return prime_list
-"""check whether a number is a prime or not
-to return true or False
-"""
-def test_if_is_a_prime(number):
-	if number > 1:
-		for j in range(2, number):
-			if not number % j:
-				return False
-		else:
-			return True
-	else:
-		return False
-		
-print (generate_prime_numbers(20))
+def generate_prime(n):
+	for num in range(2, n+1):
+		for i in range(2, num):
+			if (num % i ==0):
+				break
+	        else:
+	         	print (num)
+
+print (generate_prime(20))
