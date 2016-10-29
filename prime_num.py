78beef836ce6601095d786f1ed80b8901e3a395d
unknown <samkaris75@gmail.com>
2016-10-29 01:56:38 -0700

    modified the function to append the prime numbers for clear output as compared to the previous out that printed each prime in its own row

--- prime_num.py
+++ prime_num.py
@@ -1,9 +1,11 @@
 def generate_prime(n):
+	prime = []
 	for num in range(2, n+1):
 		for i in range(2, num):
 			if (num % i ==0):
 				break
 	        else:
-	         	print (num)
+	         	prime.append(num)
+	return prime
 
 print (generate_prime(20))
