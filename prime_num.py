fc0a53852d4f82b790f8d708dd80b9381cf02f1a
sam-karis <samkaris75@gmail.com>
2018-08-19 02:04:49 +0300

    Fix prime number function.

--- prime_num.py
+++ prime_num.py
@@ -1,11 +1,15 @@
 def generate_prime(n):
-	prime = []
-	for num in range(2, n+1):
-		for i in range(2, num):
-			if (num % i ==0):
-				break
-	        else:
-	         	prime.append(num)
-	return prime
+    prime = []
+    for num in range(2, n+1):
+        is_prime = False
+        for i in range(2, num):
+            if (num % i == 0):
+                break
+            else:
+                is_prime = True
+        if is_prime:
+            prime.append(num)
+    return prime
 
-print (generate_prime(20))
+
+print(generate_prime(20))
